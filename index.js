b0c425fb2017f5132407f754b43babf2d1ca4c2d
sepezho <sepezho@gmail.com>
2022-10-03 20:59:13 +0200

    a

---
+++ index.js
@@ -0,0 +1,35 @@
+import request from "request";
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+app.use(express.json());
+
+var allowCrossDomain = function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+
+	next();
+};
+app.use(allowCrossDomain);
+
+app.get("/", async (req, res) => {
+	res.send(
+		new Promise((resolve) =>
+			fs.readFile("./wl.json", "utf8", (err, data) => resolve(data))
+		)
+	);
+});
+
+const PORT = process.env.PORT || 8092;
+app.listen(PORT, () => {
+	console.log(`App listening on port ${PORT}`);
+	console.log("Press Ctrl+C to quit.");
+});
